17055962167f1f8b58f694157479bc0179cec36a
1 <1>
2023-08-28 19:46:34 -0400

    code

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var weatherLabel: UILabel!
+    @IBOutlet var getWeatherButoon: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getWeatherButoon.addTarget(self, action: #selector(didTapGetWeatherButton), for: .touchUpInside)
     }
 
-
+    @objc func didTapGetWeatherButton() {
+        let urlString = "https://api.open-meteo.com/v1/forecast?latitude=43.58&longitude=-79.66&current_weather=true"
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data, let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                DispatchQueue.main.async {
+                    self.weatherLabel.text = "\(weather.currentWeather.temperature) °"
+                }
+            } else {
+                print("Fail")
+            }
+        }
+        task.resume()
+    }
 }
 
